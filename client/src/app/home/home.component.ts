d10d1daf364f7a121500f31dbf70cd221558a3b2
samuelyao107 <samuelyao107@gmail.com>
2025-04-15 00:06:26 +0200

    user interface 1

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -9,13 +9,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-    http = inject(HttpClient);
+export class HomeComponent  {
+  
     registerMode = false;
     users: any;
-    ngOnInit(): void {
-      this.getUsers();
-    }
+   
     registerToggle(){
       this.registerMode = !this.registerMode;
  }
@@ -23,11 +21,5 @@ export class HomeComponent implements OnInit {
  cancelRegisterMode(event: boolean){
   this.registerMode = event;
  }
- getUsers(){
-  this.http.get('https://localhost:5001/api/users').subscribe({
-    next: response => this.users = response,
-    error: error => console.log(error),
-    complete: () => {}
-  })
-}
+
 }
