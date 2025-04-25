9bb232b161788131b511fbe05c2ea8ff02c6ffad
Samuel Yao <syao@luccasoftware.com>
2025-04-25 16:59:12 +0200

    updating user almost done we only need to add state storage service

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using AutoMapper;
+using System.Security.Claims;
+using API.Helpers;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository): BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper): BaseApiController
     {
        
         [HttpGet]
@@ -32,6 +34,21 @@ namespace API.Controllers
                 return user;
         }
         
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if(username == null) return BadRequest("No username found in token");
+
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            mapper.Map(memberUpdateDto, user);
+
+            if(await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update the user");
+        }
         
     }
 }
